ddec27741a4732c051e6827c961a81eddef7fd00
Jacob Ilagan <jilagan@kenilworth.com>
2025-10-29 14:25:24 -0400

    Delegate /tasks routes to taskRoutes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "./src/config/database.js";
 import "dotenv/config";
+import taskRoutes from "./src/routes/taskRoutes.js";
 
 await connectDB();
 const app = express();
@@ -9,6 +10,8 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Works!");
 });
+
+app.use("/tasks", taskRoutes); // delegate any routes beginning with '/tasks' to taskRoutes
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
