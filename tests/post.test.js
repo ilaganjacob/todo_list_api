f88fdf1515d38123031f555b36ac54af8485c96f
Jacob Ilagan <jilagan@kenilworth.com>
2025-10-29 15:08:02 -0400

    Add more expect statements

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,11 +1,13 @@
 import request from "supertest";
 import app from "../index";
 describe("POST /tasks", () => {
-  Test("Meta test", async () => {
+  test("Meta test", async () => {
     const data = { title: "meta test", complete: false };
-    await request(app).post("/tasks").send(data);
+
+    const res = await request(app).post("/tasks").send(data);
 
     expect(res.statusCode).toBe(201);
-    expect(res.body.status).toBe("ok");
+    expect(res.title).toBe("meta test");
+    expect(res.complete).toBe(false);
   });
 });
