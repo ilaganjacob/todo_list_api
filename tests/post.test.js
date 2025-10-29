881a15b66c928b49795b06c917ae183afcb01dcc
Jacob Ilagan <jilagan@kenilworth.com>
2025-10-29 15:27:48 -0400

    Test clean up

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,13 +1,19 @@
 import request from "supertest";
-import app from "../index";
+import mongoose from "mongoose";
+import app from "../index.js";
 describe("POST /tasks", () => {
+  // Close database connection after all tests
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
   test("Meta test", async () => {
     const data = { title: "meta test", complete: false };
 
     const res = await request(app).post("/tasks").send(data);
 
     expect(res.statusCode).toBe(201);
-    expect(res.title).toBe("meta test");
-    expect(res.complete).toBe(false);
+    expect(res.body.title).toBe("meta test");
+    expect(res.body.complete).toBe(false);
   });
 });
