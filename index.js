d23bfa4c1ae16fc3d54c8de776f92775fe153e2c
Jacob Ilagan <jilagan@kenilworth.com>
2025-10-29 12:01:33 -0400

    Delete URI and use dotenv instead

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import mongodb from "mongodb";
 import express from "express";
-require("dotenv").config();
+import "dotenv/config";
 const app = express();
 const port = 3000;
 
 const MongoClient = mongodb.MongoClient;
 
-const client = new MongoClient(uri);
-
 const dbConnection = async function (req, res, next) {
-  const uri = process.env.uri;
-  const client = new MongoClient(uri);
+  const client = new MongoClient(process.env.uri);
 
   try {
     await client.connect();
