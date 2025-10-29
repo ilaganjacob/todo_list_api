b613bbb851a124baf57bfc12e57a19d394028a2b
Jacob Ilagan <jilagan@kenilworth.com>
2025-10-29 09:15:16 -0400

    Work on connecting to DB

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import mongodb from "mongodb";
-const express = require("express");
+import express from "express";
 
 const app = express();
 const port = 3000;
+
+const MongoClient = mongodb.MongoClient;
+const uri =
+  "mongodb+srv://jacobcandoilagan:ixT60exMb7EGUrrt@jacob-cluster.j3bsxku.mongodb.net/?appName=jacob-cluster";
+
+const client = new MongoClient(uri);
+
+client.connect((err) => {
+  const collection = client.db("todo_list_api").collection("tasks");
+  // Perform any actions here
+  console.log("Connected to DB");
+  collection.insertOne({ content: "hi" });
+
+  collection.findOne({ content: "hi" });
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Works!");
